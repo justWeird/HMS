db1d373cf3746ea4a624ce718d74541b38d4855f
abumuhab <abumuhab98@gmail.com>
2019-06-03 10:24:07 +0100

    login page edits

--- HMS/src/hms/FXMLDocumentController.java
+++ HMS/src/hms/FXMLDocumentController.java
@@ -5,6 +5,7 @@
  */
 package hms;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -19,17 +20,16 @@ import javafx.scene.control.Label;
 public class FXMLDocumentController implements Initializable {
     
     @FXML
-    private Label label;
+    private JFXButton loginButton;
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+  
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }   
+
+    @FXML
+    private void loginClicked(ActionEvent event) {
+    }
     
 }
